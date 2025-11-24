dfc866c43a6ba2a412721565d72bc689dec386d2
Jeffrey Thomas <Jeffrey.Thomas@hgs.hiddengeniusproject.org>
2025-11-24 14:27:25 -0800

    b

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ app = Flask(__name__)
 
 @app.route ('/')
 def index():    
-    return "HELLO World!"
-
-
-@app.route ("/ great/ <name>")
-def Greatpage():
-    return render_template("index.html", name=name)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
